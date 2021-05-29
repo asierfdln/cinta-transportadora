b33354a244d04371640f48581f490fbbd863b963
asierfdln <asierfdln@gmail.com>
2021-05-29 17:55:59 +0200

    formal hellyiah

---
+++ gstreamer_pipelines.py
@@ -0,0 +1,55 @@
+def _gst_str(self):
+        return 'v4l2src device=/dev/video{} ! video/x-raw, width=(int){}, height=(int){}, framerate=(fraction){}/1 ! videoconvert !  video/x-raw, format=(string)BGR ! appsink'.format(self.capture_device, self.capture_width, self.capture_height, self.capture_fps)
+    
+
+"v4l2src device=/dev/video1 ! nvv4l2decoder mjpeg=1 ! nvvidconv ! video/x-raw, width=1920, height=1080, framerate=30/1, format=BGRx ! videoconvert ! video/x-raw, format=BGR ! appsink drop=1", cv2.CAP_GSTREAMER
+
+def gstreamer_pipeline_usb(
+    sensor_id=1,
+    capture_width=1920,
+    capture_height=1080,
+    framerate=30,
+):
+    return (
+        "v4l2src device=/dev/video%d ! "
+        "nvv4l2decoder mjpeg=1 ! nvvidconv ! "
+        "video/x-raw, width=%d, height=%d, framerate=%d/1, format=BGRx ! "
+        "videoconvert ! video/x-raw, format=BGR ! appsink drop=1"
+        % (
+            sensor_id,
+            capture_width,
+            capture_height,
+            framerate,
+        )
+    )
+
+def gstreamer_pipeline_csi(
+    sensor_id=0,
+    sensor_mode=3,
+    capture_width=1280,
+    capture_height=720,
+    display_width=1280,
+    display_height=720,
+    framerate=30,
+    flip_method=0,
+):
+    return (
+        "nvarguscamerasrc sensor-id=%d sensor-mode=%d ! "
+        "video/x-raw(memory:NVMM), "
+        "width=(int)%d, height=(int)%d, "
+        "format=(string)NV12, framerate=(fraction)%d/1 ! "
+        "nvvidconv flip-method=%d ! "
+        "video/x-raw, width=(int)%d, height=(int)%d, format=(string)BGRx ! "
+        "videoconvert ! "
+        "video/x-raw, format=(string)BGR ! appsink"
+        % (
+            sensor_id,
+            sensor_mode,
+            capture_width,
+            capture_height,
+            framerate,
+            flip_method,
+            display_width,
+            display_height,
+        )
+    )
--- sesuponeUSBgstreamerpipeline.py
+++
@@ -1,3 +0,0 @@
-def _gst_str(self):
-        return 'v4l2src device=/dev/video{} ! video/x-raw, width=(int){}, height=(int){}, framerate=(fraction){}/1 ! videoconvert !  video/x-raw, format=(string)BGR ! appsink'.format(self.capture_device, self.capture_width, self.capture_height, self.capture_fps)
-    
