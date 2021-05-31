90530e024df26c8def7f724ba7247f8f0e659108
asierfdln <asierfdln@gmail.com>
2021-05-31 02:15:04 +0200

    arreglau el cropping

--- jhx_csicameras.py
+++ jhx_csicameras.py
@@ -87,6 +87,8 @@ class CSI_Camera:
 # Currently there are setting frame rate on CSI Camera on Nano through gstreamer
 # Here we directly select sensor_mode 3 (1280x720, 59.9999 fps)
 def gstreamer_pipeline(
+    sensor_id=0,
+    sensor_mode=3,
     capture_width=1280,
     capture_height=720,
     display_width=1280,
@@ -95,7 +97,7 @@ def gstreamer_pipeline(
     flip_method=0,
 ):
     return (
-        "nvarguscamerasrc ! "
+        "nvarguscamerasrc sensor-id=%d sensor-mode=%d ! "
         "video/x-raw(memory:NVMM), "
         "width=(int)%d, height=(int)%d, "
         "format=(string)NV12, framerate=(fraction)%d/1 ! "
@@ -104,6 +106,8 @@ def gstreamer_pipeline(
         "videoconvert ! "
         "video/x-raw, format=(string)BGR ! appsink"
         % (
+            sensor_id,
+            sensor_mode,
             capture_width,
             capture_height,
             framerate,
@@ -139,10 +143,13 @@ def start_camera():
     left_camera = CSI_Camera()
     left_camera.open(
         gstreamer_pipeline(
-            capture_width=1920,
-            capture_height=1080,
-            display_width=1280,
-            display_height=720,
+            # capture_width=1920,
+            # capture_height=1080,
+            # display_width=1280,
+            # display_height=720,
+            # flip_method=0,
+            sensor_id=0,
+            sensor_mode=3,
             flip_method=0,
         )
         # gstreamer_pipeline_usb()
@@ -276,14 +283,14 @@ def start_csicamera_andusbcamera():
 
         _ , left_image=left_camera.read()
         left_image_rgba = cv2.cvtColor(left_image, cv2.COLOR_BGR2RGBA)
-        # jetson.utils.cudaDeviceSynchronize()
+        # jetson.utils.cudaDeviceSynchronize() # NO HACE FALTA CHECK
         left_image_cuda = jetson.utils.cudaFromNumpy(left_image_rgba)
         output_csi.Render(left_image_cuda)
         output_csi.SetStatus("Video Viewer | {:d}x{:d} | {:.1f} FPS".format(left_image_cuda.width, left_image_cuda.height, output_csi.GetFrameRate()))
 
         _ , right_image=right_camera.read()
         right_image_rgba = cv2.cvtColor(right_image, cv2.COLOR_BGR2RGBA)
-        # jetson.utils.cudaDeviceSynchronize()
+        # jetson.utils.cudaDeviceSynchronize() # NO HACE FALTA CHECK
         right_image_cuda = jetson.utils.cudaFromNumpy(right_image_rgba)
         output_usb.Render(right_image_cuda)
         output_usb.SetStatus("Video Viewer | {:d}x{:d} | {:.1f} FPS".format(right_image_cuda.width, right_image_cuda.height, output_usb.GetFrameRate()))
@@ -302,6 +309,6 @@ def start_csicamera_andusbcamera():
 
 
 if __name__ == "__main__":
-    # start_camera()
+    start_camera()
     # start_cameras()
-    start_csicamera_andusbcamera()
+    # start_csicamera_andusbcamera()
